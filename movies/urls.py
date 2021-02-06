636c35f3c1abf167cf92251466d3c8fc4d6b2a10
SadLaboka <toteshephysic@yandex.ru>
2021-02-06 23:03:41 +0300

    Переделал views и добавил шаблон для конкретного фильма

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.MovieView.as_view())
+    path("", views.MovieView.as_view(), name='home'),
+    path("<str:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MovieView(View):
+class MovieView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", context={'movies_list': movies})
+    model = Movie
+    context_object_name = 'movies_list'
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
