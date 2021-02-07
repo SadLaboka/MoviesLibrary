1e1c41b5054a4ce4c26b0e9726fb425b1be6a1b0
SadLaboka <toteshephysic@yandex.ru>
2021-02-07 19:06:21 +0300

    Добавил форму для отправки отзывов, вывод отзывов

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.MovieView.as_view(), name='home'),
     path("<str:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
+    path("review/<int:pk>", views.AddReview.as_view(), name='add_review'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MovieView(ListView):
@@ -13,3 +16,15 @@ class MovieView(ListView):
 class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
